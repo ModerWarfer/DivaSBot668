30b2244dd15449e36b2bca34d180f3f6ee89dc93
ModerWarfer <86485677+ModerWarfer@users.noreply.github.com>
2021-06-26 15:27:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,5 +14,5 @@ npm cache clean -f
 npm install --dev
 npm i got
 
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[.] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
 echo "Updates : fix Bugs"
