c5dce9259f6bbeb5f6675923f06f2b9ce8d2fee3
ModerWarfer <86485677+ModerWarfer@users.noreply.github.com>
2021-06-26 14:56:14 -0300

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,10 +1,10 @@
 const version = (prefix, pushname) => {
     return `
-*ShanBot 🤖*
+*DivaSBot 🤖*
 
 *Actualizado:* 18 de junio de 2021
-*Versión actual:* 2.6
-*Ofrecida por:* shanduy™
+*Versión actual:* 1.0
+*Ofrecida por:* ModerWarfer™
 
 
 *NOVEDADES*
@@ -15,20 +15,6 @@ Actualizacion de la version @adiwajshing/baileys a la mas estable
 
 Nuevo motor WAConnection :)
 
-
-*INFORME*
-
-Si no funciona el comando *play checa el blog del bot y mira la version que poses. 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento.
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos.
-Si vez a alguien que robo mi bot comunicate conmigo.
-
-
-*BLOG DE SHANBOT*
-
-https://github.com/shanduy/ShanBot
-
-by shanduy
 `
 
 }
